1d137020a65e1195dfb09d78997201128ac2f2b1
hamata0222 <jp.ne.hiroshi@gmail.com>
2022-09-11 17:24:07 +0900

    勝者の判定と着手済みのマスの判定を追加

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -37,6 +37,10 @@ class Board extends React.Component {
     handleClick(i) {
         // immutableな構成にしておく。後で履歴の実装とかがしやすい。
         const squares = this.state.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            // 決着がついている、もしくはすでに着手済みのマスなら何もしない。
+            return;
+        }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
             squares: squares,
@@ -54,7 +58,13 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        const winner = calculateWinner(this.state.squares);
+        let status;
+        if (winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        }
 
         return (
             <div>
@@ -95,6 +105,25 @@ class Game extends React.Component {
     }
 }
 
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && (squares[a] === squares[b]) && (squares[a] === squares[c])) {
+            return squares[a];
+        }
+    }
+    return null; // 勝者なし
+}
 // ================================================================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
